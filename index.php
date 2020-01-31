8137fae81c05e974a315f1039df5a668935ee49c
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-01-31 10:08:10 +0100

    variables and constants declarations

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// CONSTANTS
+define('NAME', 'Yoshi');
+
+// VARIABLES
+// $name = 'Yoshi';
+$age = 30;
+
+// $name = 'Mario';
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>PHP Tutorials</title>
+</head>
+
+<body>
+  <h1>User Profile Page</h1>
+
+  <div><?php echo NAME; ?></div>
+  <div><?php echo $age; ?></div>
+</body>
+
+</html>
