18631cdba73f68065cd3ba740486d5d439bd890b
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-01-31 11:17:39 +0100

    Working with strings

--- index.php
+++ index.php
@@ -1,5 +1,34 @@
 <?php
 
+$stringOne = 'my email is ';
+$stringTwo = 'mario123@thenetninja.co.uk';
+
+// STRING CONCATENATION
+// echo $stringOne . $stringTwo;
+
+$name = 'mario';
+
+// echo 'Hey, my name is ' . $name;
+
+// echo "hey my name is $name";
+
+// ESCAPING CARACTERS
+// echo "the ninja screammed \"whaaa\"";
+// echo 'the ninja screammed "whaaa"';
+
+// GETTING CARACTER FROM A STRING
+// echo $name[1];
+
+// GETTING OUT THE LENGTH OF A STRING
+// echo strlen($name);
+
+// CONVERT LOWERCASE TO UPPERCASE AND VICE VERSA
+// echo strtoupper($name);
+// echo strtolower($name);
+
+// Replaces any m by w
+echo str_replace('m', 'w', $name);
+
 
 
 ?>
