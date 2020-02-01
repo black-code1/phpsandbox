833f294b92f353581c25eb5224235b41b2e206b4
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-01 08:48:44 +0100

    working with multi-dimensional arrays

--- index.php
+++ index.php
@@ -1,6 +1,26 @@
 <?php
 
+// multi-dimensonal arays
 
+// $blogs = [
+//   ['mario party', 'mario', 'lorem', 30],
+//   ['mario kart cheats', 'toad', 'lorem', 25],
+//   ['zelda hidden chests', 'link', 'lorem', 50]
+// ];
+$blogs = [
+  ['title' => 'mario party', 'author' => 'mario', 'content' => 'lorem', 'age' => 30],
+  ['title' => 'mario kart cheats', 'author' => 'toad', 'content' => 'lorem', 'age' => 25],
+  ['title' => 'zelda hidden chests', 'author' => 'link', 'content' => 'lorem', 'age' => 50]
+];
+
+// print_r($blogs[0][1]);
+// print_r($blogs[0]['author']);
+// echo count($blogs);
+
+$blogs[] = ['title' => 'castle party', 'author' => 'peach', 'content' => 'lorem', 'likes' => 100];
+// print_r($blogs);
+$popped = array_pop($blogs);
+print_r($popped);
 
 ?>
 
