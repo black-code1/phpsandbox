33c1353953b3a2e29effc9e3efaf197c50d7d772
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-01 10:16:12 +0100

    working with booleans and comparisons

--- index.php
+++ index.php
@@ -1,5 +1,33 @@
 <?php
 
+// comparisons booleans (true or false)
+// echo true; "1"
+// echo false; ""
+
+// numbers
+// echo 5 < 10;
+// echo 5 > 10;
+// echo 5 == 10;
+// echo 10 == 10;
+// echo 5 != 10;
+// echo 5 <= 5;
+// echo 5 >= 5;
+
+// strings
+// echo 'shaun' < 'yoshi';
+// echo 'shaun' > 'yoshi';
+// echo 'shaun' > 'Shaun'; "1"
+// echo 'mario' == 'mario';
+// echo 'mario' == 'Mario';
+
+// loose vs strict equal comparison
+// echo 5 == '5'; loosely equal
+// echo 5 === '5'; stric equal comparison
+// echo 5 === 5;
+
+// echo true == "1";
+echo false == "";
+
 
 
 ?>
