4ee23f2f768a9b4d718d2bd7d3a679ab28bde570
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-01 07:59:09 +0100

    working with numbers

--- index.php
+++ index.php
@@ -1,6 +1,29 @@
 <?php
 
+$radius = 25;
+$pi = 3.14;
 
+// basic - *, /, +, -, **
+// echo $pi * $radius ** 2;
+
+// order of operation ( B I D M A S )
+// echo 2 * (4 + 9) / 3;
+
+// increment & decrement operators
+// echo $radius--;
+// echo $radius;
+
+// shorthand operators
+$age = 20;
+
+// $age -= 10;
+// $age *= 2;
+// echo $age;
+
+// number
+// echo floor($pi);
+// echo ceil($pi);
+// echo pi();
 
 ?>
 
