232b30acc04640a2bfa2e7d659ee3fe5341d67d8
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-01 10:58:35 +0100

    conditionals

--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <?php
 
+// conditional statements
 
+$price = 20;
 
+if ($price < 10) {
+  echo 'the condition is met';
+} elseif ($price < 30) {
+  echo 'elseif condition met';
+} else {
+  echo 'condition not met';
+}
 ?>
 
 <!DOCTYPE html>
