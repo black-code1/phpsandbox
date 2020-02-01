6eed1f4057cdb86391b5d978790ccf6d081692f8
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-01 11:12:05 +0100

    working with conditional statements

--- index.php
+++ index.php
@@ -4,13 +4,29 @@
 
 $price = 20;
 
-if ($price < 10) {
-  echo 'the condition is met';
-} elseif ($price < 30) {
-  echo 'elseif condition met';
-} else {
-  echo 'condition not met';
-}
+// if ($price < 10) {
+//   echo 'the condition is met';
+// } elseif ($price < 30) {
+//   echo 'elseif condition met';
+// } else {
+//   echo 'condition not met';
+// }
+
+$products = [
+  ['name' => 'shiny star', 'price' => 20],
+  ['name' => 'green shell', 'price' => 10],
+  ['name' => 'red shell', 'price' => 15],
+  ['name' => 'gold coin', 'price' => 5],
+  ['name' => 'lightning bolt', 'price' => 40],
+  ['name' => 'banana skin', 'price' => 2],
+];
+
+// foreach ($products as $product) {
+//   if ($product['price'] > 20 || $product['price'] < 10) {
+//     echo $product['name'] . '<br />';
+//   }
+// }
+// 
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +37,15 @@ if ($price < 10) {
 </head>
 
 <body>
-
+  <div>
+    <ul>
+      <?php foreach ($products as $product) { ?>
+        <?php if ($product['price'] > 15) { ?>
+          <li><?php echo $product['name'] ?></li>
+        <?php } ?>
+      <?php } ?>
+    </ul>
+  </div>
 </body>
 
 </html>
