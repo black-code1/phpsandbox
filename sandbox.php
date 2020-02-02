d7a97e005cf3c4873cb1e296e193ce4ae9120c03
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-02 19:30:42 +0100

    Open, Read, Write, ... files with file system

--- sandbox.php
+++ sandbox.php
@@ -1,32 +1,29 @@
 <?php
 
-// file system - part 1
+// file system - part 2
 
-// $quotes = readfile('readme.txt');
+$file = 'quotes.txt';
 
-// echo $quotes;
+// opening a file for reading
+// $handle = fopen($file, 'r');
+// $handle = fopen($file, 'r+');
+$handle = fopen($file, 'a+');
 
-$file = 'readme.txt';
+// read the file
+// echo fread($handle, filesize(($file)));
+// echo fread($handle, 112);
 
-if (file_exists($file)) {
+// read a single line
+// echo fgets($handle);
+// echo fgets($handle);
 
-  // read file
-  // echo readfile($file) . '<br />';
+// read a single character
+// echo fgetc($handle);
+// echo fgetc($handle);
 
-  // // copy file
-  // copy($file, 'quotes.txt');
+// writing to a file
+// fwrite($handle, "\nEverything popular is wrong");
+fclose($handle);
 
-  // // absolute path
-  // echo realpath($file) . '<br />';
-
-  // // filesize
-  // echo filesize($file) . '<br />';
-
-  // // rename file
-  // rename($file, 'test.txt');
-} else {
-  echo 'file does not exist';
-}
-
-// make directory
-mkdir('quotes');
+// Delete a file
+unlink($file);
