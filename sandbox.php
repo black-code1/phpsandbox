4e785211388cf7a0838e9beb023daa92f5a40298
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-02 17:01:12 +0100

    Ternary Operators

---
+++ sandbox.php
@@ -0,0 +1,37 @@
+<?php
+
+// ternary operators
+
+$score = 50;
+
+// if ($score > 40) {
+
+//   echo 'high score!';
+// } else {
+
+//   echo 'low score :(';
+// }
+
+// $val = $score > 40 ? 'high score' : 'low score :(';
+// echo $score > 40 ? 'high score' : 'low score :(';
+
+// echo $val;
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <title>php tuts</title>
+
+</head>
+
+<body>
+
+  <p><?php echo $score > 40 ? 'high score' : 'low score :('; ?></p>
+
+</body>
+
+</html>
