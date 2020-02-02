ad4a761344a220650d205d7f6a7cbda3dfcd91c3
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-02 18:09:19 +0100

    Creating cookies

--- templates/header.php
+++ templates/header.php
@@ -11,6 +11,9 @@ if ($_SERVER['QUERY_STRING'] == 'noname') {
 
 $name = $_SESSION['name'] ?? 'Guest';
 
+// get cookie
+$gender = $_COOKIE['gender'] ?? 'Unknown';
+
 ?>
 
 <head>
@@ -52,6 +55,7 @@ $name = $_SESSION['name'] ?? 'Guest';
       <ul id="nav-mobile" class="right hide-on-small-and-down">
 
         <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
+        <li class="grey-text">(<?php echo htmlspecialchars($gender); ?>)</li>
         <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
 
       </ul>
