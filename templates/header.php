d63b73dd203fe56ee35d3a9307e139b951f0d559
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-02 17:30:34 +0100

    working with sessions

--- templates/header.php
+++ templates/header.php
@@ -1,3 +1,20 @@
+<?php
+
+session_start();
+
+// $_SESSION['name'] = 'yoshi';
+
+if ($_SERVER['QUERY_STRING'] == 'noname') {
+  // unset($_SESSION['name']);
+
+  // clear all session variables
+  session_unset();
+}
+
+$name = $_SESSION['name'];
+
+?>
+
 <head>
   <title>Ninja Pizza</title>
   <link rel="stylesheet" href="css/materialize.min.css">
@@ -36,6 +53,7 @@
 
       <ul id="nav-mobile" class="right hide-on-small-and-down">
 
+        <li class="grey-text">Hello <?php echo htmlspecialchars($name); ?></li>
         <li><a href="add.php" class="btn brand z-depth-0">Add a Pizza</a></li>
 
       </ul>
