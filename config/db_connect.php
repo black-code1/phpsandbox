86409998a69cdd1c0f93edda56606e04dae4a415
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-02 19:59:49 +0100

    Working with Classes & Objects (part1)

--- config/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-// connect to database
-$conn = mysqli_connect('localhost', 'shaun', 'admin123', 'ninja_pizza');
-// check connection
-if (!$conn) {
-  echo 'Connection error: ' . mysqli_connect_error();
-}
--- templates/footer.php
+++
@@ -1,4 +0,0 @@
-<footer class="section">
-  <div class="center gray-text">Copyright 2019 Ninja Pizzas</div>
-</footer>
-</body>
