117700d8abbf13ba597441bccffaf16fd9332257
Mahamat Legrand Ngouh Nkainsa <legrandmahamat1@gmail.com>
2020-02-03 03:04:07 +0100

    Working with private properties in a class

--- sandbox.php
+++ sandbox.php
@@ -1,12 +1,10 @@
 <?php
 
-// classes
-
 class User
 {
 
-  public $email;
-  public $name;
+  private $email;
+  private $name;
 
   public function __construct($name, $email)
   {
@@ -20,15 +18,30 @@ class User
     // echo 'the user logged in';
     echo $this->name . ' logged in';
   }
+
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  public function setName($name)
+  {
+
+    if (is_string($name) && strlen($name) > 1) {
+
+      $this->name = $name;
+
+      return "name has been updated to $name";
+    } else {
+
+      return 'not a valid name';
+    }
+  }
 }
-
-// $userOne = new User();
-
-// $userOne->login();
-// echo $userOne->email;
 
 $userTwo = new User('yoshi', 'yoshi@thenetninja.co.uk');
 
-// echo $userTwo->name;
-// echo $userTwo->email;
-$userTwo->login();
+// echo $userTwo->getName();
+// echo $userTwo->setName(50);
+echo $userTwo->setName('shaun');
+echo $userTwo->getName();
